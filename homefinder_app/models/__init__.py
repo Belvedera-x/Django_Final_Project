44312a673ba738ca4e75416f7d17c6e81528d067
Belvedera-x <belvederastars@gmail.com>
2025-12-17 09:43:38 +0100

    update models

--- homefinder_app/models/__init__.py
+++ homefinder_app/models/__init__.py
@@ -10,4 +10,4 @@ from homefinder_app.models.booking import Booking
 from homefinder_app.models.housing import Housing
 from homefinder_app.models.image import HousingImage
 from homefinder_app.models.user import User
-from homefinder_app.models.rewiew import Review
+from homefinder_app.models.review import Review
